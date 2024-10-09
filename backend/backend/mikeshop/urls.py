9a015833d1c3bcfcd19b7fb5c7c2038cfdb6bdf9
mbpono24 <michael.beirneponomarev24@mail.dcu.ie>
2024-10-09 17:05:53 +0100

    Final Submission

--- backend/backend/mikeshop/urls.py
+++ backend/backend/mikeshop/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
    path('register/', views.register, name='register'),
    path('products/<int:prodid>/', views.product_individual, name="individual_product" ),
    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+   path('logout/', auth_views.LogoutView.as_view(template_name='index.html'), name='logout'),
    path('addbasket/<int:prodid>/', views.add_to_basket, name="add_basket"),
    path('view-basket/', views.view_basket, name='view_basket'),
    path('update-quantity/<int:item_id>/', views.update_quantity, name='update_quantity'),
    path('remove-from-basket/<int:item_id>/', views.remove_from_basket, name='remove_from_basket'),
-   path('checkout/', views.checkout, name='checkout'),
+   path('order_confirmation/', views.checkout, name='order'),
    path('orders/', views.view_orders, name='view_orders'),
    path('order-confirmation/<int:order_id>/', views.order_confirmation, name='order_confirmation')
 ]
--- backend/backend/mikeshop/views.py
+++ backend/backend/mikeshop/views.py
@@ -2,32 +2,38 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .form import *
-from django.contrib.auth import login, logout
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 
 def index(request):
     return render(request, 'index.html')
 
+
 def products(request):
     products = Product.objects.all()
     return render(request, 'all_products.html', {'products':products})
 
+
 def product_individual(request, prodid):
     product = Product.objects.get(id=prodid)    
     return render(request, 'individual_products.html', {'product':product})
+
 
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Automatically log the user in after registration
-            return redirect('index')  # Redirect to home page or any other page
+            login(request, user) 
+            return redirect('index')
     else:
         form = RegisterForm()
 
     return render(request, 'register.html', {'form': form})
 
+
+@login_required
 def add_to_basket(request, prodid):
     user = request.user
     # is there a shopping basket for the user 
@@ -52,24 +58,18 @@ def add_to_basket(request, prodid):
     return redirect("/products")
 
 
-
+@login_required
 def view_basket(request):
-    # Ensure the user is authenticated
     if not request.user.is_authenticated:
-        return redirect('/login')  # Redirect to login page if not authenticated
-    # Get the user's active basket
+        return redirect('/login') 
     user = request.user
     basket = Basket.objects.filter(user_id=user, is_active=True).first()
     if basket is None:
         return render(request, 'view_basket.html', {'empty': True})
 
-    # Get all items in the basket
     basket_items = BasketItem.objects.filter(basket_id=basket)
-
-    # Calculate total price
     total_price = sum(item.item_price() for item in basket_items)
 
-    # Pass the basket and basket items to the template
     context = {
         'basket': basket,
         'basket_items': basket_items,
@@ -78,6 +78,7 @@ def view_basket(request):
     return render(request, 'view_basket.html', context)
 
 
+@login_required
 def update_quantity(request, item_id):
     if request.method == 'POST':
         basket_item = get_object_or_404(BasketItem, id=item_id)
@@ -89,6 +90,7 @@ def update_quantity(request, item_id):
         return redirect('view_basket')
     
 
+@login_required
 def remove_from_basket(request, item_id):
     if request.method == 'POST':
         basket_item = get_object_or_404(BasketItem, id=item_id)
@@ -96,17 +98,15 @@ def remove_from_basket(request, item_id):
         return redirect('view_basket')
     
 
+@login_required
 def checkout(request):
-    # Get the user's active basket
     basket = Basket.objects.filter(user_id=request.user, is_active=True).first()
 
     if not basket:
         return redirect('view_basket')
 
-    # Calculate the total price for the basket
     total_price = sum(item.item_price() for item in basket.basketitem_set.all())
 
-    # Create the order
     order = Order.objects.create(
         user_id=request.user,
         basket_id=basket,    
@@ -115,17 +115,16 @@ def checkout(request):
     basket.is_active = False
     basket.save()
 
-    # Redirect to the order confirmation page
     return redirect('order_confirmation', order_id=order.id)
 
 
+@login_required
 def view_orders(request):
-    # Fetch the orders for the current user
     orders = Order.objects.filter(user_id=request.user).order_by('-date_ordered')
     return render(request, 'view_orders.html', {'orders': orders})
 
 
+@login_required
 def order_confirmation(request, order_id):
-    # Fetch the order based on the ID and the user
     order = Order.objects.get(id=order_id, user_id=request.user)
     return render(request, 'order_confirmation.html', {'order': order})
